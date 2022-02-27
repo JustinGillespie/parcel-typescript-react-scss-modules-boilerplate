e1f22fd5ee12ef18d7cbd0d0d20e576dce6007ff
Justin Gillespie <jgillespie@tankdesign.com>
2022-02-27 12:09:15 -0500

    test

---
+++ dist/main.js
@@ -0,0 +1,18 @@
+var $gXNCa$reactjsxruntime = require("react/jsx-runtime");
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "Coffee", () => $ef1ce4d0051b590d$export$2e2bcd8739ae039);
+
+function $ef1ce4d0051b590d$export$2e2bcd8739ae039() {
+    return(/*#__PURE__*/ $gXNCa$reactjsxruntime.jsx("div", {
+        children: "Coffee is Wicked Delicious!"
+    }));
+}
+
+
+
+
+//# sourceMappingURL=main.js.map
---
+++ dist/module.js
@@ -0,0 +1,14 @@
+import {jsx as $5OpyM$jsx} from "react/jsx-runtime";
+
+
+function $7c5a5a432c0e2418$export$2e2bcd8739ae039() {
+    return(/*#__PURE__*/ $5OpyM$jsx("div", {
+        children: "Coffee is Wicked Delicious!"
+    }));
+}
+
+
+
+
+export {$7c5a5a432c0e2418$export$2e2bcd8739ae039 as Coffee};
+//# sourceMappingURL=module.js.map
